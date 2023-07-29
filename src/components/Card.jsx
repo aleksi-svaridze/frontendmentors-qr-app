8e8d573e34b59e49afd0ec93fcb8cf55d5f4395e
aleksi svaridze <aleksisvaridze@outlook.com>
2023-07-29 11:32:16 +0400

    Version for testing

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,11 @@ const Card = () => {
     return (
         <div className="card">
             <div className="card__image-content">
-                <img src={qr_code} alt="" />
+                <img src={qr_code} alt="" className='card__image-content--qrcode' />
+            </div>
+            <div className='card__text-content'>
+                <h2 className='card__text-content--title'>Improve your front-end skills by building projects</h2>
+                <p className='card__text-content--desc'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
             </div>
         </div>
     )
