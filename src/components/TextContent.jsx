711b77565a3fdb18cece7ce970f59405a16d13f1
aleksi-svaridze <aleksisvaridze@outlook.com>
2024-12-29 14:24:25 +0400

    Add extra padding to textcontent component

--- src/components/TextContent.jsx
+++ src/components/TextContent.jsx
@@ -3,7 +3,7 @@
 export default function TextContent(props) {
     const {title, desc} = props.data;
   return (
-    <section className='text-center px-[12px]'>
+    <section className='text-center px-[10px]'>
         <h2 className='text-[#1F314F] text-[22px] leading-[120%] font-bold mb-4'>{title}</h2>
         <p className='text-[#68778D] text-[15px] leading-[140%]'>{desc}</p>
     </section>
